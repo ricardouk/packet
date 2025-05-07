ricardouk/packet

commit 42f58fabe79262d5239a4f654af615dad7f32bc8

Use `formatx` to give more context to formatted gettext msgid strings
`formatx` is a library that does runtime string interpolation.

src/window.rs
@@ -2,6 +2,7 @@ use std::path::PathBuf;
 
 use adw::prelude::*;
 use adw::subclass::prelude::*;
+use formatx::formatx;
 use gettextrs::{gettext, ngettext};
 use gtk::glib::clone;
 use gtk::{gio, glib};
@@ -211,12 +212,12 @@ impl QuickShareApplicationWindow {
                                     .set_visible_child_name("send_nearby_devices_page");
 
                                 let title_fmt = ngettext(
-                                    "One file is ready to send",
-                                    "files are ready to send",
+                                    "{} file is ready to send",
+                                    "{} files are ready to send",
                                     files.n_items(),
                                 );
                                 let title = if files.n_items() > 1 {
-                                    format!("{} {}", files.n_items(), title_fmt)
+                                    formatx!(&title_fmt, files.n_items()).unwrap_or_default()
                                 } else {
                                     title_fmt
                                 };
@@ -310,12 +311,16 @@ impl QuickShareApplicationWindow {
 
                     let file_count = file_transfer_state.filenames().len();
                     (
-                        format!(
-                            "{} {} {file_count} {}",
-                            device_name,
-                            gettext("wants to share"),
-                            ngettext("file", "files", file_count as u32)
-                        ),
+                        formatx!(
+                            ngettext(
+                                "{} wants to share {} file",
+                                "{} wants to share {} files",
+                                file_count as u32
+                            ),
+                            &device_name,
+                            file_count
+                        )
+                        .unwrap_or_default(),
                         device_name,
                     )
                 }
@@ -330,12 +335,15 @@ impl QuickShareApplicationWindow {
                     // FIXME: there's probably a better way to do this right?
                     // As it is, translators might have issues translating this due to loss of context
                     (
-                        format!(
-                            "{} {file_count} {} {}",
-                            gettext("Ready to share"),
-                            ngettext("file", "files", file_count as u32),
-                            gettext("to this device")
-                        ),
+                        formatx!(
+                            ngettext(
+                                "Ready to share {} file to this device",
+                                "Ready to share {} files to this device",
+                                file_count as u32
+                            ),
+                            file_count
+                        )
+                        .unwrap_or_default(),
                         device_name,
                     )
                 }

Cargo.toml
@@ -31,3 +31,4 @@ tokio = { version = "1.45.0", features = [
 whoami = "1.6.0"
 async-channel = "2.3.1"
 directories = "6.0.0"
+formatx = "0.2.4"

commit 7050eb98b5135da29defe483fe21d0c11dbc9329

Fix non-formatted string regression from 42f58fa

src/window.rs
@@ -211,16 +211,15 @@ impl QuickShareApplicationWindow {
                                     .get()
                                     .set_visible_child_name("send_nearby_devices_page");
 
-                                let title_fmt = ngettext(
-                                    "{} file is ready to send",
-                                    "{} files are ready to send",
-                                    files.n_items(),
-                                );
-                                let title = if files.n_items() > 1 {
-                                    formatx!(&title_fmt, files.n_items()).unwrap_or_default()
-                                } else {
-                                    title_fmt
-                                };
+                                let title = formatx!(
+                                    &ngettext(
+                                        "{} file is ready to send",
+                                        "{} files are ready to send",
+                                        files.n_items(),
+                                    ),
+                                    files.n_items()
+                                )
+                                .unwrap_or_default();
 
                                 imp.selected_files_card_title.get().set_label(&title);
 