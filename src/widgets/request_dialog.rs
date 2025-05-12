a352af4910d6da092e6357231402ef30b5634a94
nozwock <57829219+nozwock@users.noreply.github.com>
2025-05-12 18:12:12 +0530

    Improve "Done" view for received text data
    Have the retrieved text be in a GtkTextView, with a a "Copy to
    clipboard" button in the AdwHeaderBar.
    
    A "Open" button for Url type text which'll open the url in the
    registered default app.
    
    Wifi not implemented currently due to lib not exposing the required
    info.

--- src/widgets/request_dialog.rs
+++ src/widgets/request_dialog.rs
@@ -4,7 +4,11 @@ use adw::prelude::*;
 use adw::subclass::prelude::*;
 use formatx::formatx;
 use gettextrs::{gettext, ngettext};
-use gtk::{glib, glib::clone};
+use gtk::{
+    gio,
+    glib::{self, clone},
+};
+use rqs_lib::hdl::TextPayloadType;
 
 use crate::{objects, utils, window::QuickShareApplicationWindow};
 
@@ -44,11 +48,11 @@ pub fn create_receive_request_dialog(
     let root_box = gtk::Box::builder()
         .orientation(gtk::Orientation::Vertical)
         .valign(gtk::Align::Center)
-        .margin_top(12)
-        .margin_bottom(12)
-        .margin_start(12)
-        .margin_end(12)
-        .spacing(12)
+        .margin_top(18)
+        .margin_bottom(18)
+        .margin_start(18)
+        .margin_end(18)
+        .spacing(18)
         .build();
     clamp.set_child(Some(&root_box));
 
@@ -101,6 +105,81 @@ pub fn create_receive_request_dialog(
         .build();
     consent_box.append(&consent_decline_button);
     consent_box.append(&consent_accept_button);
+
+    let text_view = gtk::TextView::builder()
+        .top_margin(12)
+        .bottom_margin(12)
+        .left_margin(12)
+        .right_margin(12)
+        .editable(false)
+        .monospace(true)
+        .cursor_visible(false)
+        .wrap_mode(gtk::WrapMode::Word)
+        .build();
+
+    let text_view_frame = gtk::Frame::builder()
+        .child(&gtk::ScrolledWindow::builder().child(&text_view).build())
+        .visible(false)
+        .build();
+    root_box.append(&text_view_frame);
+
+    let copy_text_button = gtk::Button::builder()
+        .valign(gtk::Align::Center)
+        .hexpand(true)
+        .height_request(50)
+        .icon_name("edit-copy-symbolic")
+        .tooltip_text(&gettext("Copy to clipboard"))
+        .css_classes(["circular", "flat"])
+        .visible(false)
+        .build();
+    let open_uri_button = gtk::Button::builder()
+        .halign(gtk::Align::Center)
+        .valign(gtk::Align::Center)
+        .hexpand(true)
+        .height_request(50)
+        .label(&gettext("Open"))
+        .css_classes(["pill", "suggested-action"])
+        .visible(false)
+        .build();
+    header_bar.pack_end(&copy_text_button);
+    root_box.append(&open_uri_button);
+
+    let clipboard = win.clipboard();
+
+    copy_text_button.connect_clicked(clone!(
+        #[weak]
+        text_view,
+        #[strong]
+        clipboard,
+        move |_| {
+            let text = text_view.buffer().text(
+                &text_view.buffer().start_iter(),
+                &text_view.buffer().end_iter(),
+                false,
+            );
+            clipboard.set_text(&text);
+        }
+    ));
+
+    open_uri_button.connect_clicked(clone!(
+        #[weak]
+        win,
+        #[weak]
+        text_view,
+        move |_| {
+            let url = text_view.buffer().text(
+                &text_view.buffer().start_iter(),
+                &text_view.buffer().end_iter(),
+                false,
+            );
+
+            gtk::UriLauncher::new(&url).launch(
+                win.root().and_downcast_ref::<adw::ApplicationWindow>(),
+                None::<gio::Cancellable>.as_ref(),
+                |_err| {},
+            );
+        }
+    ));
 
     consent_decline_button.connect_clicked(clone!(
         #[weak(rename_to = rqs)]
@@ -198,11 +277,10 @@ pub fn create_receive_request_dialog(
         }
     ));
 
+    let msg = receive_state.borrow().msg.clone();
     // Setting initial state for WaitingForUserContent
     {
         dialog.present(imp.obj().root().as_ref());
-
-        let msg = receive_state.borrow().msg.clone();
 
         consent_accept_button.set_sensitive(true);
 
@@ -234,9 +312,9 @@ pub fn create_receive_request_dialog(
             .unwrap_or_default()
         } else {
             formatx!(
-                gettext("{} wants to share <i>\"{}\"</i>"),
+                gettext("{} wants to share <i>{}</i>"),
                 msg.get_device_name(),
-                msg.get_text_data().unwrap().description
+                msg.get_text_data().unwrap().description.replace("\n", "")
             )
             .unwrap_or_default()
         };
@@ -244,6 +322,7 @@ pub fn create_receive_request_dialog(
         caption_label.set_label(&caption);
     }
 
+    let init_id = msg.id.clone();
     glib::spawn_future_local(clone!(
         #[strong]
         receive_state,
@@ -252,11 +331,16 @@ pub fn create_receive_request_dialog(
             let rx = event_receiver;
 
             loop {
-                let msg = rx.recv().await.unwrap();
+                let msg = rx.recv().await;
 
-                receive_state.borrow_mut().msg = msg.clone();
+                if let Ok(msg) = &msg {
+                    receive_state.borrow_mut().msg = msg.clone();
+                }
 
-                if let Some(ref state) = msg.state {
+                if let Some((state, msg)) = msg
+                    .ok()
+                    .and_then(|it| Some((it.state.clone().unwrap_or(State::Initial), it)))
+                {
                     match state {
                         State::Initial => {}
                         State::ReceivedConnectionRequest => {}
@@ -332,11 +416,16 @@ pub fn create_receive_request_dialog(
                             caption_label.set_visible(true);
                             consent_box.set_visible(false);
                             header_bar.set_show_end_title_buttons(true);
+                            copy_text_button.set_visible(true);
 
-                            title_label.set_label(&gettext("Done"));
-                            let finished_text = {
+                            title_label.set_visible(false);
+                            dialog.set_title(&gettext("Done"));
+                            toolbar_view.set_extend_content_to_top_edge(false);
+                            root_box.set_margin_top(0);
+
+                            {
                                 if let Some(files) = msg.get_filenames() {
-                                    formatx!(
+                                    let text = formatx!(
                                         ngettext(
                                             "Received {} file",
                                             "Received {} files",
@@ -344,16 +433,53 @@ pub fn create_receive_request_dialog(
                                         ),
                                         files.len()
                                     )
-                                    .unwrap_or_default()
+                                    .unwrap_or_default();
+
+                                    caption_label.set_label(&text);
                                 } else {
-                                    formatx!(
-                                        gettext("Received text <i>\"{}\"</i>"),
-                                        msg.get_text_data().unwrap().description
-                                    )
-                                    .unwrap_or_default()
+                                    // FIXME: Can't handle WiFi shares yet
+                                    // TextPayloadInfo not exposed by the library
+                                    let text_type = msg.get_text_data().unwrap().kind.unwrap();
+
+                                    if text_type.clone() as u32 == TextPayloadType::Url as u32 {
+                                        open_uri_button.set_visible(true);
+                                    } else {
+                                        open_uri_button.set_visible(false);
+                                    }
+
+                                    // FIXME: add a "save as" button to save text view content
+                                    // as a file
+
+                                    let _text = msg.get_text_data().unwrap().text;
+                                    let text = if text_type.clone() as u32
+                                        == TextPayloadType::Text as u32
+                                    {
+                                        let text = _text.trim();
+                                        &text[1..text.len() - 1] // Remove quotes put in there by the lib :(
+                                    } else {
+                                        &_text
+                                    };
+
+                                    caption_label.set_label(&format!(
+                                        "Received {}",
+                                        format!("{:?}", text_type)
+                                    ));
+                                    text_view_frame.set_visible(true);
+                                    text_view.set_buffer(Some(
+                                        &gtk::TextBuffer::builder().text(text).build(),
+                                    ));
+
+                                    if text_type.clone() as u32 == TextPayloadType::Wifi as u32 {
+                                        caption_label.set_label(
+                                            &formatx!(
+                                                &gettext("{}, not implemented yet :("),
+                                                &caption_label.label()
+                                            )
+                                            .unwrap(),
+                                        );
+                                    }
                                 }
                             };
-                            caption_label.set_label(&finished_text);
 
                             break;
                         }
